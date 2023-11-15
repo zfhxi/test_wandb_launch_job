8eb8f0482f538f8802138771ed27c4c078ba918e
zfhxi <zifeihanxi@hotmail.com>
2023-11-15 13:58:07 +0800

    2222

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import time
 WORKSPACE = os.path.dirname(os.path.abspath(__file__))
 if __name__=="__main__":
     wandb.init(project='TEST',name=time.strftime("%m%d_%H%M%S", time.localtime()))
-    wandb.log({'seed':1111})
+    wandb.log({'seed':2222})
     wandb.finish()
     print(WORKSPACE)
